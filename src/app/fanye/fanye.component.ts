5a252604c889cce2c66cbe990cccb21a72104eee
yef3 <yef3@rpi.edu>
2021-04-02 09:43:41 -0400

    5 files modified

--- src/app/fanye/fanye.component.ts
+++ src/app/fanye/fanye.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
 
 @Component({
-  selector: 'app-fanye',
-  templateUrl: './fanye.component.html',
-  styleUrls: ['./fanye.component.css']
+  selector: "app-fanye",
+  templateUrl: "./fanye.component.html",
+  styleUrls: ["./fanye.component.css"]
 })
 export class FanyeComponent implements OnInit {
+  constructor(private httpService: HttpService) {}
+  ngOnInit(): void {}
 
-  constructor() { }
+  public read() {
+    this.httpService.sendCityName("Beijing").subscribe(data => {
+      (<HTMLInputElement>document.querySelector("#timezone1")).innerHTML =
+        data["timezone"];
+    });
 
-  ngOnInit(): void {
+    this.httpService.sendCityName("London").subscribe(data => {
+      (<HTMLInputElement>document.querySelector("#timezone2")).innerHTML =
+        data["timezone"];
+    });
+
+    this.httpService.sendCityName("Barcelona").subscribe(data => {
+      (<HTMLInputElement>document.querySelector("#timezone3")).innerHTML =
+        data["timezone"];
+    });
   }
 
+  public refresh() {
+    read();
+  }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor( private httpClient : HttpClient) { }
-  
   // for testing purpose
   public sendGetRequest(u: string) {
-    return this.httpClient.post('/api', '', {params: {userid: u}})
+    return this.httpClient.post("/api", "", { params: { userid: u } });
+  }
+  // Return city timezone
+  public sendCityName(u: string) {
+    return this.httpClient.get("/getCityTime", { params: { city_name: u } });
   }
 }
